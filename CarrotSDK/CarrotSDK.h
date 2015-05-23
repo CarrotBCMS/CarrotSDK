63b8f28519168e0d72c87cc2b294fd03876f2f30
Heiko Dreyer <mail@boxedfolder.com>
2015-05-23 22:30:47 +0200

    Initial Commit

---
+++ CarrotSDK/CarrotSDK.h
@@ -0,0 +1,19 @@
+//
+//  CarrotSDK.h
+//  CarrotSDK
+//
+//  Created by Heiko Dreyer on 23.05.15.
+//  Copyright (c) 2015 boxedfolder.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CarrotSDK.
+FOUNDATION_EXPORT double CarrotSDKVersionNumber;
+
+//! Project version string for CarrotSDK.
+FOUNDATION_EXPORT const unsigned char CarrotSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CarrotSDK/PublicHeader.h>
+
+
