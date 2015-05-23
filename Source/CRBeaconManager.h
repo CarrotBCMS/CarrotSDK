557347aef26188a2d8da1bac5ef44ce20b79820d
Heiko Dreyer <mail@boxedfolder.com>
2015-05-23 22:55:55 +0200

    Added first method stubs

--- Source/CRBeaconManager.h
+++ Source/CRBeaconManager.h
@@ -7,7 +7,46 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <CoreLocation/CoreLocation.h>
+
+#import "CRBeacon.h"
+
+@class CRBeaconManager;
+@protocol CRBeaconManagerDelegate <NSObject>
+@optional
+
+-(void)manager:(CRBeaconManager *)beaconManager
+didEnterRegion:(CLBeaconRegion *)region
+        beacon:(CRBeacon *)beacon;
+
+- (void)manager:(CRBeaconManager *)beaconManager
+  didExitRegion:(CLBeaconRegion *)region
+         beacon:(CRBeacon *)beacon;
+
+- (void)manager:(CRBeaconManager *)beaconManager didDetermineState:(CLRegionState)state
+      forRegion:(CLBeaconRegion *)region;
+
+- (void)manager:(CRBeaconManager *)beaconManager didRangeBeacons:(NSArray *)beacons
+       inRegion:(CLBeaconRegion *)region;
+
+@end
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////
 
 @interface CRBeaconManager : NSObject
+
+-(instancetype)initWithDelegate: (id<CRBeaconManagerDelegate>)delegate;
+
+-(void)handleAuthorization;
+
+-(void)startRangingBeacons;
+
+-(void)stopRangingBeacons;
+
+-(BOOL)startSyncingProcessWithError: (NSError **)error;
+
+-(BOOL)cancelSyncingProcess;
+
 
 @end
