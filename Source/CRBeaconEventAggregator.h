55ed2c6cd59a8d7e05cb3399e837bf1c7ecb1ed6
Heiko Dreyer <mail@boxedfolder.com>
2015-07-19 15:47:50 +0200

    Added nonnull prop

--- Source/CRBeaconEventAggregator.h
+++ Source/CRBeaconEventAggregator.h
@@ -2,11 +2,13 @@
 //  CRBeaconEventAggregator.h
 //  CarrotSDK
 //
-//  Created by Heiko Dreyer on 18.07.15.
+//  Created by Heiko Dreyer on 07/18/15.
 //  Copyright (c) 2015 boxedfolder.com. All rights reserved.
 //
 
 #import "CRSingleFileStorage.h"
+
+NS_ASSUME_NONNULL_BEGIN
 
 @interface CRBeaconEventAggregator : CRSingleFileStorage
 
@@ -15,3 +17,5 @@
 - (NSArray *)beaconsForEvent:(NSUInteger)eventId;
 
 @end
+
+NS_ASSUME_NONNULL_END
