8678bd5efaa81f9380494ecaca97fff2d94752ae
Heiko Dreyer <mail@boxedfolder.com>
2015-12-13 19:07:59 +0100

    Added static library support

---
+++ CarrotSDKStatic/CarrotSDKStatic.h
@@ -0,0 +1,13 @@
+//
+//  CarrotSDKStatic.h
+//  CarrotSDKStatic
+//
+//  Created by Heiko Dreyer on 12/13/15.
+//  Copyright © 2015 boxedfolder.com. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CarrotSDKStatic : NSObject
+
+@end
---
+++ CarrotSDKStatic/CarrotSDKStatic.m
@@ -0,0 +1,13 @@
+//
+//  CarrotSDKStatic.m
+//  CarrotSDKStatic
+//
+//  Created by Heiko Dreyer on 12/13/15.
+//  Copyright © 2015 boxedfolder.com. All rights reserved.
+//
+
+#import "CarrotSDKStatic.h"
+
+@implementation CarrotSDKStatic
+
+@end
