7ff2987d1c3702c1cdca872349618dbfad97089f
Heiko Dreyer <hdreyer@uandi.com>
2015-12-22 14:03:51 +0100

    Removed comments / Static junk

--- Source/CarrotSDK.h
+++ Source/CarrotSDK.h
@@ -18,13 +18,9 @@
 
 #import <UIKit/UIKit.h>
 
-//! Project version number for CarrotSDK.
 FOUNDATION_EXPORT double CarrotSDKVersionNumber;
-
-//! Project version string for CarrotSDK.
 FOUNDATION_EXPORT const unsigned char CarrotSDKVersionString[];
 
-// In this header, you should import all the public headers of your framework using statements like #import <CarrotSDK/PublicHeader.h>
 #import <CarrotSDK/CRBeaconManager.h>
 #import <CarrotSDK/CRBeaconManagerDelegate.h>
 #import <CarrotSDK/CRBeacon.h>
